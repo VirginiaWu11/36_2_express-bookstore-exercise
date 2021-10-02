5b028d41732200851d06d8e83b7eb4f2b3507679
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-10-02 15:04:18 -0400

    add tests, do not allow additional properties

--- data.sql
+++ data.sql
@@ -1,4 +1,5 @@
-\c books 
+\c books
+-- \c books_test
 DROP TABLE IF EXISTS books;
 
 CREATE TABLE books (
@@ -13,3 +14,4 @@ CREATE TABLE books (
 );
 
 INSERT INTO books (isbn,amazon_url,author,language) VALUES ('1542007062','https://www.amazon.com/dp/B084RNNCF8/ref=apubna_ab_gs_28665_tx_qu_gi_2109','Melinda Leigh','English')
+
